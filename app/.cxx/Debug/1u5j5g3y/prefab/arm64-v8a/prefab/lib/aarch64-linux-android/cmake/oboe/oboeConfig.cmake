1d9885c34bc79581f1b67bf4c34fff36cf0139e6
Chabdile <138411051+Chabdile@users.noreply.github.com>
2024-10-11 13:46:24 +0900

    backed up

--- app/.cxx/Debug/1u5j5g3y/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/oboe/oboeConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET oboe::oboe)
-add_library(oboe::oboe SHARED IMPORTED)
-set_target_properties(oboe::oboe PROPERTIES
-    IMPORTED_LOCATION "/Users/x21008xx/.gradle/caches/transforms-4/0bab344ebe757f849118e3ead79c4486/transformed/oboe-1.9.0/prefab/modules/oboe/libs/android.arm64-v8a/liboboe.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/Users/x21008xx/.gradle/caches/transforms-4/0bab344ebe757f849118e3ead79c4486/transformed/oboe-1.9.0/prefab/modules/oboe/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
--- app/.cxx/RelWithDebInfo/qw5q4395/prefab/x86/prefab/lib/i686-linux-android/cmake/oboe/oboeConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET oboe::oboe)
-add_library(oboe::oboe SHARED IMPORTED)
-set_target_properties(oboe::oboe PROPERTIES
-    IMPORTED_LOCATION "/Users/x21008xx/.gradle/caches/transforms-4/175556e54fd77ebeeef845220b07f679/transformed/oboe-1.9.0/prefab/modules/oboe/libs/android.x86/liboboe.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/Users/x21008xx/.gradle/caches/transforms-4/175556e54fd77ebeeef845220b07f679/transformed/oboe-1.9.0/prefab/modules/oboe/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
